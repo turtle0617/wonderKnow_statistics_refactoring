646dba70cc36d97fdd0029075ad812239232f557
turtle0617 <qqaz50313@gmail.com>
2019-01-15 13:59:28 +0800

    feature fb goodidea fans page like button and GA

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
     <div class="nav__separate">|</div>
     <router-link to="/android">Android</router-link>
   </div>
+  <div class="fb-like" data-href="https://www.facebook.com/GoodideasStudio/" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true"></div>
   <router-view/>
   <div class="scrollTop" v-on:click="scrollTop">
       <!-- <i class="fas fa-sort-up"></i> -->
